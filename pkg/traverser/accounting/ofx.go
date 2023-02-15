e189bcf3e9d92840a6c8ea73bcc93cfbc6c5ba1f
tjayrush <jrush@quickblocks.io>
2023-02-14 20:56:05 -0500

    Adding a few test cases

---
+++ pkg/traverser/accounting/ofx.go
@@ -0,0 +1,36 @@
+package accounting
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/colors"
+	"github.com/TrueBlocks/trueblocks-traversers/pkg/mytypes"
+	"github.com/TrueBlocks/trueblocks-traversers/pkg/traverser"
+)
+
+// --------------------------------
+type OfxWriter struct {
+	Opts  traverser.Options
+	Value uint64
+}
+
+func (c *OfxWriter) Traverse(r *mytypes.RawReconciliation) {
+	// SendToOfx(connection, r)
+}
+
+func (c *OfxWriter) GetKey(r *mytypes.RawReconciliation) string {
+	return ""
+}
+
+func (c *OfxWriter) Result() string {
+	return c.Name() + "\n\t" + c.reportValue("OfxWriter: ", c.Value)
+}
+
+func (c *OfxWriter) Name() string {
+	return colors.Green + reflect.TypeOf(c).Elem().String() + colors.Off
+}
+
+func (c *OfxWriter) reportValue(msg string, v uint64) string {
+	return fmt.Sprintf("%s%d", msg, v)
+}
